bdcef59ca33d0aba380bc74e75edf60cfa5f1caf
Abdul Juma <karimjuma9820@gmail.com>
2022-09-09 10:36:32 -0700

    prints lowercase alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry Point
  * prints the alphabet in lowercase, followed by a new line
- * Return 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
