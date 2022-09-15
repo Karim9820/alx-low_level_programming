cbaf65c1ee726de70ccd5d9ca9638dd6da1106c2
Abdul Juma <karimjuma9820@gmail.com>
2022-09-15 12:39:29 -0700

    Fizz Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,19 +18,30 @@ int main(void)
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else
+		{
 			printf("%d", i);
+		}
 
 		if (i != 100)
-			printf(" ");		}
+		{
+			printf(" ");
+		}
 
 		i++;
 	}
+
 	printf("\n");
 	return (0);
 }
