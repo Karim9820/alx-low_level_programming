8ebe0405c8e69febf59d9095d4c5c88beea34a56
Abdul Juma <karimjuma9820@gmail.com>
2022-09-22 14:36:46 +0000

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  * *_strcat - concatenates two strings
  * @src: source string
  * @dest: destination string
- * Returns:  pointer to the resulting string dest
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
