461e82fb524ace925573cb66e7cdde2f96c90321
Abdul Juma <karimjuma9820@gmail.com>
2022-09-28 18:52:32 +0000

    power of x to y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,14 +10,15 @@
 
 int _pow_recursion(int x, int y)
 {
-	int power = x;
-
-	power ^= _pow_recursion(x, y);
-
 	if (y < 0)
+	{
 		return (-1);
-	else if (y == 0)
-		return (1);
+	}
 	else
-		return (power);
+	{
+		if (y > 0)
+			return (x * (_pow_recursion(x, y - 1)));
+		else
+			return (1);
+	}
 }
