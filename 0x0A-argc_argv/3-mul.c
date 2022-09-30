849e41e78507de3c98030ff3893b68806da4c826
Abdul Juma <karimjuma9820@gmail.com>
2022-09-30 15:12:42 +0000

    multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *main - multiplies two numbers
+ *@argc: argument counter
+ *@argv: argument array
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, k;
+
+	i = 1;
+	j = 2;
+	k = 3;
+
+	if (argc != k)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[i]) * atoi(argv[j]));
+	return (0);
+}
