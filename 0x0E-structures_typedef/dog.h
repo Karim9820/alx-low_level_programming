c467fbb3f0c63f59a627fc49a2d33b97b5bcb6b3
Abdul Juma <karimjuma9820@gmail.com>
2022-10-11 16:55:25 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ *struct dog - dog class
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: name of dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+;
+
+#endif
